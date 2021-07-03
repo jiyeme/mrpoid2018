f1e9e80aec7fcb3ac9dd307e8c2f0974879fbc9e
jiye <jiyecafe@gmail.com>
2021-07-03 22:51:28 +0800

    update:SDK 28

--- app/src/main/java/com/mrpoid/mrplist/app/BaseActivity.java
+++ app/src/main/java/com/mrpoid/mrplist/app/BaseActivity.java
@@ -3,7 +3,6 @@ package com.mrpoid.mrplist.app;
 import android.os.Build;
 import android.os.Build.VERSION_CODES;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.Window;
